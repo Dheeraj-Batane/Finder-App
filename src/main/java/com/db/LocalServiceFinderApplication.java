104c4af2843851968f7389b6ec41476681d45fce
Dheeraj Batane <dheerajbatane84@gmail.com>
2026-03-29 18:45:09 +0530

    enable async programming in application

--- src/main/java/com/db/LocalServiceFinderApplication.java
+++ src/main/java/com/db/LocalServiceFinderApplication.java
@@ -2,10 +2,12 @@ package com.db;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(exclude={
 org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
 })
+@EnableAsync
 public class LocalServiceFinderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LocalServiceFinderApplication.class, args);
